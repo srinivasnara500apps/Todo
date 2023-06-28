705ecba84eb4a22b522388565af4d5e80c29027c
srinivasnara500apps <137782978+srinivasnara500apps@users.noreply.github.com>
2023-06-28 19:34:43 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue';
+import Todolist from "./components/Todolist.vue"
+</script>
+
+<template>
+ <!-- <HelloWorld/> -->
+ <Todolist/>
+</template>
+
