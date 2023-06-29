74bf0b0f77f3bc0df7bb3883731e875ef2f7aeab
srinivasnara500apps <137782978+srinivasnara500apps@users.noreply.github.com>
2023-06-29 12:21:51 +0530

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+</script>
+
+<template>
+  <h1 class="bg-blue-300">{{ message }}</h1>
+</template>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+// import Vue from 'vue'
+// import VueCookies from 'vue-cookies'
+
+// Vue.use(VueCookies);
